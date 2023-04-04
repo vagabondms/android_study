bd3371e6202e2551300fe243d3887d2f845433b1
vagabondms <hiseokseok@gmail.com>
2023-04-04 20:26:02 +0900

    button disable 기능 구현

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,7 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true))
     private var currentIndex = 0
+    private var disabled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +37,9 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
+
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
@@ -49,21 +54,69 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+        val question = questionBank[currentIndex]
+        questionTextView.setText(question.textResId)
+        disabled = question.right
     }
+
+    private fun updateButtonStatus(enable: Boolean){
+        trueButton.isEnabled = enable
+        falseButton.isEnabled = enable
+    }
+
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
 
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+
+        val messageResId = when(userAnswer == correctAnswer) {
+            true -> {
+                questionBank[currentIndex].right  = true
+                updateQuestion()
+                updateButtonStatus(!disabled)
+                R.string.correct_toast
+            }
+            false -> {
+                questionBank[currentIndex].right  = false
+                updateQuestion()
+                updateButtonStatus(!disabled)
+                R.string.incorrect_toast
+            }
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
